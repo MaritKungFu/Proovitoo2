9debd652feae6abe62838c063f396788037795b8
MaritKungFu <68182080+MaritKungFu@users.noreply.github.com>
2020-07-14 10:24:57 +0300

    Removed password

--- NUnitTestProject1/KunglaTests.cs
+++ NUnitTestProject1/KunglaTests.cs
@@ -16,7 +16,7 @@ namespace KunglaProovitoo
 		public void ValidateLogo()
 		{
 			string logo = wait.Until(ExpectedConditions.ElementExists(By.XPath("//div/p[@class='logo']"))).Text;
-			Assert.AreEqual("okidoki", logo, "Lehe pealkiri ei ole okidoki.");
+			Assert.AreEqual("okidoki", logo, "Logo text is not okidoki.");
 		}
 
 		[OneTimeSetUp]
@@ -33,20 +33,21 @@ namespace KunglaProovitoo
 		}
 
 		[Test, Order(1)]
-		[TestCase("Testitestimine", "ckHD4d4a4TibTvN")]
+		// Add valid password.
+		[TestCase("Testitestimine", "", Description = "It is possible to log in with correct username and password")]
 		public void LogIn(string username, string password)
 		{
 			ValidateLogo();
 
 			// Find and click on "Logi sisse" button.
-			Console.WriteLine("Kliki avalehel logi sisse nupule.");
+			Console.WriteLine("Click log in button on homepage.");
 			driver.FindElement(By.XPath("//a[@href='/login/']")).Click();
 
 			ValidateLogo();
 
 			// Find h1 and validate value.
 			string header = wait.Until(ExpectedConditions.ElementIsVisible(By.TagName("h1"))).Text;
-			Assert.AreEqual("Sisene okidoki keskkonda", header, "H1 ei ole korrektne.");
+			Assert.AreEqual("Sisene okidoki keskkonda", header, "H1 is not correct.");
 
 			// Find "Kasutajanimi" field and insert username.
 			driver.FindElement(By.Id("login-input")).SendKeys(username);
@@ -57,28 +58,33 @@ namespace KunglaProovitoo
 			// Find "logi sisse" button and click
 			driver.FindElement(By.XPath("//*[@value='Logi sisse']")).Click();
 
+			Assert.IsEmpty((driver.FindElements(By.XPath("//*[@id='login']/div/h1/following-sibling::*[1]"))), "Error visible");
+
 			ValidateLogo();
 
 			// Validate that user is loged in and log off button becomes visible.
 			string logi_valja = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='homepage']/div[1]/div/div[1]/p/a[2]"))).Text;
-			Assert.AreEqual(logi_valja, "Logi välja", "Logi välja nupp ei ole nähtav.");
+			Assert.AreEqual(logi_valja, "Logi välja", "Log out button is not visible.");
 			string loggedInUser = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='homepage']/div[1]/div/div[1]/p/a[1]"))).Text;
-			Assert.AreEqual(username, loggedInUser, "Sisse logitud kasutajanimi on vale.");
+			Assert.AreEqual(username, loggedInUser, "Logged in username is not correct.");
 
 			// Log off and validate that log in button becomes visible.
-			Console.WriteLine("Kliki logi välja nupule.");
+			Console.WriteLine("Click log off button.");
 			wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='homepage']/div[1]/div/div[1]/p/a[2]"))).Click();
 			ValidateLogo();
 			string logi_sisse = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@href='/login/']"))).Text;
-			Assert.AreEqual(logi_sisse, "Logi sisse", "Logi sisse nupp ei ole nähtav.");
+			Assert.AreEqual(logi_sisse, "Logi sisse", "Log in button is not visible.");
 		}
 
 		[Test, Order (2)]
-		[TestCase("", "", "Sisesta oma kasutajanimi\r\nSisesta oma parool")]
-		[TestCase("Testitestimine", "", "Sisesta oma parool")]
-		[TestCase("", "ckHD4d4a4TibTvN", "Sisesta oma kasutajanimi")]
-		[TestCase("Marit", "ckHD4d4a4TibTvN", "Sisestasid vale kasutajanime või parooli")]
-		[TestCase("Testitestimine", "pauh", "Sisestasid vale kasutajanime või parooli")]
+		// Empty fields.
+		[TestCase("", "", "Sisesta oma kasutajanimi\r\nSisesta oma parool", Description = "It is not possible to log in with empty fields.")]
+		[TestCase("Testitestimine", "", "Sisesta oma parool", Description = "It is not possible to log in with one empty password field.")]
+		// Add correct password leave username empty.
+		[TestCase("", "", "Sisesta oma kasutajanimi", Description = "It is not possible to log in with empty username field.")]
+		// Add correct password.
+		[TestCase("Marit", "", "Sisestasid vale kasutajanime või parooli", Description = "It is not possible to log in with wrong username.")]
+		[TestCase("Testitestimine", "pauh", "Sisestasid vale kasutajanime või parooli", Description = "It is not possible to log in with wrong password.")]
 		public void FailToLogIn(string username, string password, string message)
 		{
 			driver.Url = "https://www.okidoki.ee/login/";
@@ -87,7 +93,7 @@ namespace KunglaProovitoo
 
 			// Find h1 and validate value.
 			string header= wait.Until(ExpectedConditions.ElementIsVisible(By.TagName("h1"))).Text;
-			Assert.AreEqual("Sisene okidoki keskkonda", header, "H1 ei ole korrektne.");
+			Assert.AreEqual("Sisene okidoki keskkonda", header, "H1 is not correct.");
 
 			// Find "Kasutajanimi" field and insert username.
 			driver.FindElement(By.Id("login-input")).SendKeys(username);
@@ -100,7 +106,7 @@ namespace KunglaProovitoo
 
 			// Validate that error appears and user is not loged in.
 			string error = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='login']/div/h1/following-sibling::*[1]"))).Text;
-			Assert.That(error.Contains(message), "Error ei ole nähtav.");
+			Assert.That(error.Contains(message), "Error is not correct.");
 		}
 
 		[OneTimeTearDown]
